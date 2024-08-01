31aff38bab7aa9a0b367f34ecae0d5416147fa7b
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-08-01 13:19:41 +0200

    Fix eslint

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <MantineProvider defaultColorScheme="dark">
       <MainPage />
     </MantineProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
