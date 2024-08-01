8bf1cb5a4393c39c401efc317d3528a678e42712
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-08-01 18:30:31 +0200

    Add text

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,15 @@
 import TeamCard from "@/components/teams/TeamCard";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
-import { Container, Grid } from "@mantine/core";
+import {
+  Container,
+  Grid,
+  Stack,
+  Title,
+  Text,
+  Flex,
+  Button,
+} from "@mantine/core";
 import Header from "@/components/layout/Header";
 
 export default function MainPage() {
@@ -11,13 +19,20 @@ export default function MainPage() {
     <>
       <Header />
       <Container fluid={true} py={30}>
-        <Grid justify="center" gutter="lg">
-          {currentGrid.map((team) => (
-            <Grid.Col key={team.id} span={{ md: 12, lg: 6, xl: 6 }}>
-              <TeamCard team={team} />
-            </Grid.Col>
-          ))}
-        </Grid>
+        <Stack gap={20}>
+          <Title ta={"center"}>My Silly Season</Title>
+          <Flex align={"center"} justify="center" gap="10">
+            <Text>Fill the drivers for each team and </Text>
+            <Button>Generate your grid</Button>
+          </Flex>
+          <Grid justify="center" gutter="lg">
+            {currentGrid.map((team) => (
+              <Grid.Col key={team.id} span={{ md: 12, lg: 6, xl: 6 }}>
+                <TeamCard team={team} />
+              </Grid.Col>
+            ))}
+          </Grid>
+        </Stack>
       </Container>
     </>
   );
